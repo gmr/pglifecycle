e7a870740c0017580a691a5fd21829b3aaea9c1c
Gavin M. Roy <gavinr@aweber.com>
2019-10-17 17:45:17 -0400

    Tiny change to force tests to run

--- pglifecycle/utils.py
+++ pglifecycle/utils.py
@@ -5,5 +5,5 @@ Misc Utilities
 
 
 def quote_ident(value: str) -> str:
-    """Quote a PostgreSQL identifier"""
+    """Quote a PostgreSQL identifier (object name, etc)"""
     return '"{}"'.format(value.replace('"', '""'))
