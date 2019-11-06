59b8a7438398d5df52ed939d612019114e17797f
Gavin M. Roy <gavinr@aweber.com>
2019-11-06 10:21:43 -0500

    Write a function for quoting postgres values

--- pglifecycle/utils.py
+++ pglifecycle/utils.py
@@ -2,8 +2,21 @@
 Misc Utilities
 
 """
+import typing
 
 
 def quote_ident(value: str) -> str:
     """Quote a PostgreSQL identifier (object name, etc)"""
     return '"{}"'.format(value.replace('"', '""'))
+
+
+def postgres_value(value: typing.Any, nested: bool = False) -> str:
+    """Return a Postgres value as a string, quoted if required, etc."""
+    if isinstance(value, str):
+        if "'" in value:
+            return '$${}$$'.format(value)
+        return "'{}'".format(value)
+    if isinstance(value, list):
+        return ('[{}]' if nested else 'ARRAY[{}]').format(
+            ', '.join(postgres_value(v, True) for v in value))
+    return str(value)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,3 +10,23 @@ class QuoteIdentTestCase(unittest.TestCase):
 
     def test_double_quotes(self):
         self.assertEqual(utils.quote_ident('foo"bar"baz'), '"foo""bar""baz"')
+
+
+class PostgresValueTestCase(unittest.TestCase):
+
+    def test_int(self):
+        self.assertEqual(utils.postgres_value(1), '1')
+
+    def test_str(self):
+        self.assertEqual(utils.postgres_value('foo'), "'foo'")
+
+    def test_str_with_single_quote(self):
+        self.assertEqual(utils.postgres_value("foo'bar"), "$$foo'bar$$")
+
+    def test_array(self):
+        self.assertEqual(utils.postgres_value(["foo'bar", 'baz']),
+                         "ARRAY[$$foo'bar$$, 'baz']")
+
+    def test_nested_array(self):
+        self.assertEqual(utils.postgres_value([[1, 2, 3], [4, 5, 6]]),
+                         'ARRAY[[1, 2, 3], [4, 5, 6]]')
